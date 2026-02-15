84aad8c70a2cf16ff8e15ce1276f441c3ac365bd
Xhope <morabryan322@gmail.com>
2026-02-15 12:54:16 -0500

    cambios simples en home-page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,13 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/book.interface';
 import { LibrosService } from '../../services/libros.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent implements OnInit {
+  authService = inject(AuthService); // Inyección
+
   private librosService = inject(LibrosService);
   libros: Book[] = [];
 
@@ -19,5 +23,9 @@ export class HomePageComponent implements OnInit {
       },
       error: (err) => console.error('Error al cargar libros', err),
     });
+  }
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
